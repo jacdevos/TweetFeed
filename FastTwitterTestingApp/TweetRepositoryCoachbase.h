1ad5cec0f6226bd5e534c3a5a5a2416910c0ba49
Jacques de Vos <jacdevos@gmail.com>
2015-11-21 18:43:48 +0200

    Counchbase document db initial files

---
+++ FastTwitterTestingApp/TweetRepositoryCoachbase.h
@@ -0,0 +1,13 @@
+//
+//  TweetRepositoryCoachbase.h
+//  SpeedTweetRead
+//
+//  Created by jacdevos on 2015/11/21.
+//  Copyright © 2015 nReality. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TweetRepositoryCoachbase : NSObject
+
+@end
---
+++ FastTwitterTestingApp/TweetRepositoryCoachbase.m
@@ -0,0 +1,13 @@
+//
+//  TweetRepositoryCoachbase.m
+//  SpeedTweetRead
+//
+//  Created by jacdevos on 2015/11/21.
+//  Copyright © 2015 nReality. All rights reserved.
+//
+
+#import "TweetRepositoryCoachbase.h"
+
+@implementation TweetRepositoryCoachbase
+
+@end
