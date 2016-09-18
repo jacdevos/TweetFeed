f8a746c09c6dbca740dd569b706f29872cb1e826
Jacques de Vos <jacdevos@gmail.com>
2016-09-18 14:08:21 +0200

    Cleanup TweetDownloader before changes

--- FastTwitterTestingApp/TweetDownloader.swift
+++ FastTwitterTestingApp/TweetDownloader.swift
@@ -2,26 +2,14 @@ import TwitterKit
 typealias TweetsDownloadedCallback = (NSArray?, NSError?) -> Void
 
 class TweetDownloader {
+    static let countPerCall = 200
+    static let numberOfCalls = 3
+    
     static func downloadHomeTimelineTweets(_ callback : @escaping TweetsDownloadedCallback){
-        //TODO: need to cater for 2 types of calls:
-        //the one that gets latest 200
-        //the other that gets from the last downloaded ID up to latest or 200 cap
-        
-        let statusesShowEndpoint = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let params = ["count":"200","exclude_replies":"false"]
-        var clientError : NSError?
-        
-        let request = TWTRAPIClient.withCurrentUser().urlRequest(
-            withMethod: "GET",
-            url: statusesShowEndpoint,
-            parameters: params,
-            error: &clientError)
-        
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         
-        TWTRAPIClient.withCurrentUser().sendTwitterRequest(request) { (response, data, connectionError) -> Void in
-            UIApplication.shared.isNetworkActivityIndicatorVisible = false
-            
+        TWTRAPIClient.withCurrentUser().sendTwitterRequest(buildTwitterRequest()) { (response, data, connectionError) -> Void in
+
             if (connectionError == nil && data != nil) {
                 let tweetArray = getJSONTweetArrayFromData(data!)
                 callback(tweetArray,nil)
@@ -30,9 +18,22 @@ class TweetDownloader {
                 print("Error calling twitter: \(connectionError)")
                 callback(nil, connectionError as NSError?)
             }
+            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
         }
     }
     
+    static func buildTwitterRequest()->URLRequest{
+        let statusesShowEndpoint = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let params = ["count":"\(countPerCall)","exclude_replies":"false"]
+        var clientError : NSError?
+        
+        return TWTRAPIClient.withCurrentUser().urlRequest(
+            withMethod: "GET",
+            url: statusesShowEndpoint,
+            parameters: params,
+            error: &clientError)
+    }
     
     fileprivate static func getJSONTweetArrayFromData(_ data: Data) -> NSArray {
         var jsonError : NSError?
